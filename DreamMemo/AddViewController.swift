ff5593d8fc05834cb228670e97345ba8fa14c488
kamoni <mt99peacefulkoala@icloud.com>
2017-11-12 15:50:11 +0900

    夢日記用アプリ

---
+++ DreamMemo/AddViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AddViewController.swift
+//  DreamMemo
+//
+//  Created by 田邊範子 on 2017/11/12.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func save(){
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- DreamMemo/ViewController.swift
+++ DreamMemo/ViewController.swift
@@ -7,16 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return contents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        return cell
+    }
+    
+    
+    @IBOutlet var table:UITableView!
+    var contents: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        table.dataSource = self
+        table.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func add(){
+        
     }
 
 
