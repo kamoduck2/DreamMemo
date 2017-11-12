395b8b28ea39beb4758e6f27f0a06b66c4c62eb9
kamoni <mt99peacefulkoala@icloud.com>
2017-11-12 12:27:30 +0900

    Initial Commit

---
+++ DreamMemo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  DreamMemo
+//
+//  Created by 田邊範子 on 2017/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
